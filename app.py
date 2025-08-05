ef8e0a5ec1f640e5358e1b4ed522a95b8eb4e114
TimHolman <holman.timothyj@gmail.com>
2025-08-05 10:42:39 -0400

    Add live updating date and time page

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask
-from datetime import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    now = datetime.now()
-    current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-    return f"The current date and time is: {current_time}"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
